21200e14b30811d96f36a1ac123b3923897c4033
MaximilianGeorgiev <scissorhands.tw@gmail.com>
2021-10-21 10:52:52 +0300

    Amend tests.

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,12 +1,10 @@
-const app = require("../src/index");
 const axios = require('axios');
 const jestOpenAPI = require('jest-openapi').default;
 //const swaggerDocument = require('../swagger.json');
 //jestOpenAPI(swaggerDocument);
-require('dotenv').config();
 
 //const server = "http://" + process.env.SERVER_HOST + ":" + process.env.SERVER_PORT;
-const server = "http://localhost:3001";
+const server = "http://localhost:8080";
 console.log(server);
 jest.setTimeout(20000);
 
@@ -15,40 +13,40 @@ describe('GET /', () => {
         const res = await axios.get(server + "/");
 
         expect(res.status).toEqual(200);
-        expect(res.data).toEqual("Hello World!");
+        expect(res.data).toContain("<html>"); // main page for Actionhero is a html page 
     });
 });
 
-describe('GET /blocks/latest', () => {
+describe('GET api/blocks/latest', () => {
     it('should satisfy OpenAPI spec', async () => {
-        const res = await axios.get(server + '/blocks/latest');
+        const res = await axios.get(server + '/api/blocks/latest');
 
         expect(res.status).toEqual(200);
         // expect(res).toSatisfyApiSpec();
     });
 });
 
-describe('GET /blocks/ByNumber/OrHash/:arg', () => {
+describe('GET api/blocks/ByNumber/OrHash/:arg', () => {
     it('should satisfy OpenAPI spec', async () => {
-        const res = await axios.get(server + '/blocks/ByNumber/OrHash/1000');
+        const res = await axios.get(server + '/api/blocks/ByNumber/OrHash/1000');
 
         expect(res.status).toEqual(200);
         //expect(res).toSatisfyApiSpec();
     });
 });
 
-describe('GET /blocks/{from}/{count}', () => {
+describe('GET api/blocks/{from}/{count}', () => {
     it('should satisfy OpenAPI spec', async () => {
-        const res = await axios.get(server + '/blocks/1000/2');
+        const res = await axios.get(server + '/api/blocks/1000/2');
 
         expect(res.status).toEqual(200);
         //expect(res).toSatisfyApiSpec();
     });
 });
 
-describe('GET /blocks/pending', () => {
+describe('GET api/blocks/pending', () => {
     it('should satisfy OpenAPI spec', async () => {
-        const res = await axios.get(server + '/blocks/pending');
+        const res = await axios.get(server + '/api/blocks/pending');
 
         expect(res.status).toEqual(200);
         //expect(res).toSatisfyApiSpec();
@@ -56,19 +54,9 @@ describe('GET /blocks/pending', () => {
 });
 
 
-describe('GET /transactions/hash/{hash}', () => {
+describe('GET api/transactions/hash/{hash}', () => {
     it('should satisfy OpenAPI spec', async () => {
-        const res = await axios.get(server + '/transactions/hash/0xfddf5bcce2beafd53f0bd20271ae50b7b31f917cb24c117528201f7527cff783');
-
-        expect(res.status).toEqual(200);
-        //expect(res).toSatisfyApiSpec();
-
-    });
-});
-
-describe('GET /transactions/pending/', () => {
-    it('should satisfy OpenAPI spec', async () => {
-        const res = await axios.get(server + '/transactions/pending');
+        const res = await axios.get(server + '/api/transactions/hash/0xfddf5bcce2beafd53f0bd20271ae50b7b31f917cb24c117528201f7527cff783');
 
         expect(res.status).toEqual(200);
         //expect(res).toSatisfyApiSpec();
@@ -76,9 +64,9 @@ describe('GET /transactions/pending/', () => {
     });
 });
 
-describe('GET /transactions/address/{address}', () => {
+describe('GET api/transactions/pending/', () => {
     it('should satisfy OpenAPI spec', async () => {
-        const res = await axios.get(server + '/transactions/address/0x52Add4435c81a4e0fB2eC494966863e48BF9302E');
+        const res = await axios.get(server + '/api/transactions/pending');
 
         expect(res.status).toEqual(200);
         //expect(res).toSatisfyApiSpec();
@@ -86,45 +74,55 @@ describe('GET /transactions/address/{address}', () => {
     });
 });
 
-describe('GET /transactions/latest', () => {
+describe('GET api/transactions/address/{address}', () => {
     it('should satisfy OpenAPI spec', async () => {
-        const res = await axios.get(server + '/transactions/latest');
+        const res = await axios.get(server + '/api/transactions/address/0x52Add4435c81a4e0fB2eC494966863e48BF9302E');
+
+        expect(res.status).toEqual(200);
+        //expect(res).toSatisfyApiSpec();
+
+    });
+});
+
+describe('GET api/transactions/latest', () => {
+    it('should satisfy OpenAPI spec', async () => {
+        const res = await axios.get(server + '/api/transactions/latest');
 
         expect(res.status === 200 || res.status === 204).toBe(true);
         //expect(res).toSatisfyApiSpec();
     });
 });
 
-describe('GET /addresses/{address}', () => {
+describe('GET api/addresses/{address}', () => {
     it('should satisfy OpenAPI spec', async () => {
-        const res = await axios.get(server +'/addresses/0x52Add4435c81a4e0fB2eC494966863e48BF9302E');
+        const res = await axios.get(server +'/api/addresses/0x52Add4435c81a4e0fB2eC494966863e48BF9302E');
 
         expect(res.status).toEqual(200);
         //expect(res).toSatisfyApiSpec();
     });
 });
 
-describe('GET /transactions/hash/{hash}', () => {
+describe('GET api/transactions/hash/{hash}', () => {
     it('should return null', async () => {
-        const res = await axios.get(server + '/transactions/hash/0xfddf5bcce2beafd53f0bd20271ae50b7b31f917cb24c117528201f7527cff78a');
+        const res = await axios.get(server + '/api/transactions/hash/0xfddf5bcce2beafd53f0bd20271ae50b7b31f917cb24c117528201f7527cff78a');
 
-        expect(res.status).toEqual(204);
-        expect(res.data).toBe("");
+        expect(res.status).toEqual(200);
+        expect(res.data.response).toBe(null);
     });
 });
 
-describe('GET /transactions/address/{address}', () => {
+describe('GET api/transactions/address/{address}', () => {
     it('should return undefined', async () => {
-        const res = await axios.get(server +'/transactions/address/ax52Add4435c81a4e0fB2eC494966863e48BF9302E')
+        const res = await axios.get(server +'/api/transactions/address/ax52Add4435c81a4e0fB2eC494966863e48BF9302E')
             .catch((error) => {
             });
-        expect(res.data).toBe("Invalid address.");
+        expect(res).toBe(undefined);
     });
 });
 
-describe('GET /blocks/ByNumber/OrHash/:arg', () => {
+describe('GET api/blocks/ByNumber/OrHash/:arg', () => {
     it('should return undefined', async () => {
-        const res = await axios.get(server + '/blocks/ByNumber/OrHash/1000a')
+        const res = await axios.get(server + '/api/blocks/ByNumber/OrHash/1000a')
             .catch((error) => {
             });
 
@@ -132,9 +130,9 @@ describe('GET /blocks/ByNumber/OrHash/:arg', () => {
     });
 });
 
-describe('GET /addresses/{address}', () => {
+describe('GET api/addresses/{address}', () => {
     it('should return undefined', async () => {
-        const res = await axios.get(server + '/addresses/0x1111111111111')
+        const res = await axios.get(server + '/api/addresses/0x1111111111111')
             .catch((error) => {
             });
 
